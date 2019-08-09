885321f7476326a0f811fe0040e7c6e68058b59f
Moe Myat Zaw <mmzliveid@gmail.com>
2019-08-09 19:27:42 +0630

    Update zg2uni rules

--- modules/zawgyi-unicode-translit-rules/src/zg2uni-rules.ts
+++ modules/zawgyi-unicode-translit-rules/src/zg2uni-rules.ts
@@ -14,6 +14,7 @@ export const zg2uniRules: TranslitRule = {
     phases: [
         {
             tplVar: {
+                '#zc': '\u1000-\u1021\u1025\u1027\u1040-\u1049\u106A\u106B\u1086\u108F\u1090',
                 '#z3bOr7eTo84': '\u103B\u107E-\u1084',
             },
             tplSeq: {
@@ -29,49 +30,53 @@ export const zg2uniRules: TranslitRule = {
                         // description: "'န' သို့",
                         from: '\u108F',
                         to: '\u1014',
-                        orGroup: 'a'
+                        start: -1,
+                        orGroup: 'gc1'
                     },
                     {
                         // description: "'ရ' သို့",
                         from: '\u1090',
                         to: '\u101B',
-                        orGroup: 'a'
+                        start: -1,
+                        orGroup: 'gc1'
                     },
                     {
                         // description: "'ဉ' သို့",
                         from: '\u106A',
-                        // TODO: \u1025?
                         to: '\u1009',
-                        orGroup: 'a'
+                        start: -1,
+                        orGroup: 'gc1'
                     },
                     {
                         // description: "'ည' မှ",
                         from: '\u106B',
                         to: '\u100A',
-                        orGroup: 'a'
+                        start: -1,
+                        orGroup: 'gc1'
                     },
+
                     {
                         // description: "သုည မှ 'ဝ' သို့",
                         from: '\u1040',
                         to: '\u101D',
-                        orGroup: 'a'
+                        start: -1,
+                        orGroup: 'gc1'
                     }
                 ]
             },
             rules: [
-                // (zg: \u108B-\u108D)
+                // (zg: \u108B-\u108D) -> \u102D, \u102E, \u1036
                 // ------------------------------------------------------------------------------------------
                 // Pasin
                 // ...............
-                //
-                //
+                // 'ေ' + 'ြ' + 'တ' + 'ွ'
                 {
                     from: '\u1031[#z3bOr7eTo84]([#zc])\u1096#zkx',
                     to: '\u1004\u103A\u1039$1\u1039\u1010\u103C\u103D\u1031#zkx',
                     minLength: 5,
                     quickTests: [['\u1031', 0], ['\u1096', 3], ['#zkx', 4]],
                     postRulesRef: 'pz2u',
-                    postRulesStart: { a: 3}
+                    postRulesStart: { gc1: 3 }
                 },
             ]
         }
