08b4a588bc993113ffa9891a95d980e06a458870
Moe Myat Zaw <mmzliveid@gmail.com>
2019-08-09 19:27:10 +0630

    Update tests

---
+++ modules/zawgyi-unicode-translit-rules/test/zg2uni.spec.ts
@@ -0,0 +1,103 @@
+// tslint:disable: no-floating-promises
+
+// tslint:disable: no-implicit-dependencies
+
+import { TestBed } from '@angular/core/testing';
+
+import { TranslitModule, TranslitResult, TranslitService } from '@dagonmetric/ng-translit';
+
+import { zg2uniRules } from '../src/zg2uni-rules';
+
+export function formatCodePoints(str?: string): string {
+    if (str == null) {
+        return '';
+    }
+
+    const cpArray: string[] = [];
+    for (const c of str) {
+        const cp = c.codePointAt(0);
+        if (cp != null && /[\u1000-\u109F\uAA60-\uAA7F\uA9E0-\uA9FF]/.test(c)) {
+            cpArray.push(`\\u${cp.toString(16)}`);
+        } else if (/[\b\f\n\r\t\v]/.test(c)) {
+            if (c === '\n') {
+                cpArray.push('\\n');
+            } else if (c === '\r') {
+                cpArray.push('\\r');
+            } else if (c === '\t') {
+                cpArray.push('\\t');
+            } else if (c === '\f') {
+                cpArray.push('\\f');
+            } else if (c === '\v') {
+                cpArray.push('\\v');
+            } else if (c === '\b') {
+                cpArray.push('\\b');
+            }
+        } else {
+            cpArray.push(`${c}`);
+        }
+    }
+
+    return cpArray.join('');
+}
+
+export function toFailOutput(input: string, result: TranslitResult): string {
+    let str = `\n\ninput: ${formatCodePoints(input)}\n`;
+    str += `outputText: ${formatCodePoints(result.outputText)}\n\n`;
+
+    if (result.traces) {
+        for (const trace of result.traces) {
+            str += `from (parsed): ${formatCodePoints(trace.parsedFrom)}\n`;
+            str += `from: ${formatCodePoints(trace.from)}\n`;
+            str += `to: ${formatCodePoints(trace.to)}\n`;
+            str += `inputString: ${formatCodePoints(trace.inputString)}\n`;
+            str += `matchedString: ${formatCodePoints(trace.matchedString)}\n`;
+            str += `replacedString: ${formatCodePoints(trace.replacedString)}\n`;
+
+            if (trace.postRuleTraces && trace.postRuleTraces.length > 0) {
+                str += 'post rules:\n';
+                for (const subTrace of trace.postRuleTraces) {
+                    str += `from: ${formatCodePoints(subTrace.from)}\n`;
+                    str += `to: ${formatCodePoints(subTrace.to)}\n`;
+                    str += `inputString: ${formatCodePoints(subTrace.inputString)}\n`;
+                    str += `matchedString: ${formatCodePoints(subTrace.matchedString)}\n`;
+                    str += `replacedString: ${formatCodePoints(subTrace.replacedString)}\n`;
+                }
+            }
+            str += '\n';
+        }
+    }
+
+    return str;
+}
+
+describe('uni2zg-rules-individual', () => {
+    let translitService: TranslitService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                TranslitModule.withOptions({
+                    trace: true,
+                    shareCachedRules: false
+                })
+            ]
+        });
+
+        translitService = TestBed.get<TranslitService>(TranslitService) as TranslitService;
+    });
+
+    // (zg: \u108B-\u108D) -> \u102D, \u102E, \u1036
+    // ------------------------------------------------------------------------------------------
+    // Pasin
+    // ...............
+    it("should work with '\u1031[#z3bOr7eTo84]([#zc])\u1096#zkx'", (done: DoneFn) => {
+        const input = '\u1031\u1083\u108F\u1096\u108B';
+        const expected = 'င်္န္တြွေိ';
+
+        translitService.translit(input, 'zg2uni', zg2uniRules)
+            .subscribe(result => {
+                expect(result.outputText).toBe(expected, toFailOutput(input, result));
+                done();
+            });
+    });
+});
